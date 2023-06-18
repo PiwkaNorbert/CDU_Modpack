1d0684904b7617017e8a0d3d849f73705c86c86f
PiwkaNorbert <piwkanorbert@gmail.com>
2023-06-18 14:29:47 +0200

    ugabuga

---
+++ edit/API/useDiscordProfileData.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const useDiscordProfileData = () => {
+
+  const fetchProfile = async () => {
+
+    const { data, status } = await axios.get(`/profile`, {
+      withCredentials: true,
+    });
+    if (status !== 200) throw new Error("No login data found");
+    console.log(data);
+    
+    return data 
+  };
+
+  return useQuery(["login"], fetchProfile, {
+    onError: (_err: Error) =>  {
+      console.error(_err);
+      toast.error("Sorry, there was an error logging you in!")
+    },
+    onSuccess: () => toast.success("Welcome back! You are now logged in!")
+  });
+};
+
+export default useDiscordProfileData;
---
+++ edit/components/LogoutButton.tsx
@@ -0,0 +1,17 @@
+import { toast } from "react-toastify";
+
+export const LogoutButton = () => {
+    const returnLink = window.location.href
+    return(
+        <button
+            className=" text-content flex h-8 items-center gap-2 rounded-lg px-3 py-1 ml-4 bg-slate-500 dark:hover:bg-hover-2 hover:bg-hover-1 "
+            onClick={() => {
+                localStorage.removeItem("user_profile");
+                window.location.href = returnLink;
+               return toast.success("See you later!");
+            }}
+            >
+            Logout
+        </button>
+    )
+};
