afb852a897984813672024656feef06394501883
Trainjumper1 <trainjumperx@gmail.com>
2023-06-26 09:06:10 +0000

    Resolve merge

--- src/Components/LogoutButton.tsx
+++ src/Components/LogoutButton.tsx
@@ -1,17 +1,17 @@
 import { toast } from "react-toastify";
 
 export const LogoutButton = () => {
-  const returnLink = window.location.href;
-  return (
-    <button
-      className=" text-content ml-4 flex h-8 items-center gap-2 rounded-lg bg-slate-500 px-3 py-1 hover:bg-hover-1 dark:hover:bg-hover-2 "
-      onClick={() => {
-        localStorage.removeItem("user_profile");
-        window.location.href = returnLink;
-        return toast.success("See you later!");
-      }}
-    >
-      Logout
-    </button>
-  );
+    const returnLink = window.location.href
+    return(
+        <button
+            className=" text-content flex h-8 items-center gap-2 rounded-lg px-3 py-1 ml-4 bg-sec dark:hover:bg-hover-2 hover:bg-hover-1 "
+            onClick={() => {
+                localStorage.removeItem("user_profile");
+                window.location.href = returnLink;
+               return toast.success("See you later!");
+            }}
+            >
+            Log out
+        </button>
+    )
 };
