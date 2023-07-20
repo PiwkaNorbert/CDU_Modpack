3f496636578a129675300eb03a48d105df82d574
Norbert Piwka <piwkanorbert@gmail.com>
2023-07-20 14:17:21 +0200

    no proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "https://api.playcdu.co": {
-        target: "https://api.playcdu.co",
-        changeOrigin: true,
-      },
-    },
-  },
 });
