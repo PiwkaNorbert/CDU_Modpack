555c20e6ab748d6dcc0e8c6e50441e6aa379578d
Norbert Piwka <piwkanorbert@gmail.com>
2023-08-04 13:02:43 +0200

    dialog for images

---
+++ src/Components/Dialog.tsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export interface DialogProps {
+  allowClose?: boolean;
+  contents: React.ReactNode;
+  open: boolean;
+  dialogStateChange?: (open: boolean) => void;
+}
+
+export function Dialog({
+  allowClose = true,
+  contents,
+  open,
+  dialogStateChange = () => {},
+}: DialogProps) {
+  const [showModal, setShowModal] = useState(open);
+  const dialog = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (open !== showModal) {
+      setShowModal(open);
+    }
+  }, [open]);
+
+  function updateDialogState(open: boolean) {
+    setShowModal(open);
+    dialogStateChange(open);
+  }
+
+  return showModal ? (
+    <>
+      <div className="fixed inset-0 z-40 bg-bg/70 "></div>
+      <div
+        onClick={({ target }) => {
+          if (!allowClose || dialog.current?.contains(target as any)) {
+            return;
+          }
+          updateDialogState(false);
+        }}
+        onKeyDown={({ key }) => {
+          if (!allowClose || key !== "Escape") {
+            return;
+          }
+          updateDialogState(false);
+        }}
+        className="dialog-backdrop "
+      >
+        <div className="dialog-placement">
+          <div className="group relative">
+            <div className="dialog-accent-border group-hover:duration-2000 group-hover:opacity-100"></div>
+            <div
+              ref={dialog}
+              className="dialog-content-container bg-white/70  text-bg dark:bg-black/70 dark:text-text"
+            >
+              {contents}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  ) : null;
+}
+
+export interface DropDownProps {
+  allowClose?: boolean;
+  contents: React.ReactNode;
+  open: boolean;
+  dropDownStateChange?: (open: boolean) => void;
+}
+
+export function DropDown({
+  allowClose = true,
+  contents,
+  open,
+  dropDownStateChange = () => {},
+}: DropDownProps) {
+  const [showDropDown, setShowDropDown] = useState(open);
+  const dropdown = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (open !== showDropDown) {
+      setShowDropDown(open);
+    }
+  }, [open]);
+
+  function updateDropDownState(open: boolean) {
+    setShowDropDown(open);
+    dropDownStateChange(open);
+  }
+
+  return showDropDown ? (
+    <>
+      <div
+        onClick={({ target }) => {
+          if (!allowClose || dropdown.current?.contains(target as any)) {
+            return;
+          }
+          updateDropDownState(false);
+        }}
+        onKeyDown={({ key }) => {
+          if (!allowClose || key !== "Escape") {
+            return;
+          }
+          updateDropDownState(false);
+        }}
+        className="dialog-backdrop z-40"
+      >
+        <div
+          className="w-min-content absolute right-2 top-[67px]  rounded-xl border border-text/20 bg-bg text-text shadow"
+          ref={dropdown}
+        >
+          {contents}
+        </div>
+      </div>
+    </>
+  ) : null;
+}
