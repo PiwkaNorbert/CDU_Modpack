1a4850bd9d8baefae0c4634117fbe8fb729b5ba7
Norbert Piwka <piwkanorbert@gmail.com>
2023-09-05 11:27:24 +0200

    cleaning up dropdowns + debugging

--- src/Components/ModpackListView.tsx
+++ src/Components/ModpackListView.tsx
@@ -53,85 +53,89 @@ const ModpackListView = ({ packData }) => {
         <div className=" h-min overflow-hidden border-t-2 bg-bg dark:border-none dark:bg-bg dark:shadow  md:mb-4 md:rounded-b-xl  md:border-none md:shadow-2xl  ">
           {/* map the data variable in a grad 4x2  */}
           <div className="md:space-x-none  flex items-center  justify-between   space-x-4 p-5 text-xl text-text xl:text-2xl ">
-            <div className=" relative z-20">
+            {/* Show this button if you're logged in and a staff member */}
+            <div className=" relative  z-10 w-96">
               {location.pathname === "/archived-modpacks" && "Archived "}
               {location.pathname === "/suggested-modpacks" && "Suggested "}
               Modpacks
-              <button
-                className="w-12"
-                id="dropdown-button"
-                data-dropdown-toggle="dropdown"
-                type="button"
-                ref={menuButtonRef}
-                onClick={() => {
-                  setShowModal((open) => !open);
-                }}
-              >
-                V
-              </button>
               {user?.isAdmin && (
-                <DropDown
-                  open={showModal}
-                  dropDownStateChange={(open: any) => setShowModal(open)}
-                  contents={
-                    <ul
-                      className="space-y-1 p-1 text-sm"
-                      aria-labelledby="dropdown-button"
-                    >
-                      <li
-                        onClick={() => {
-                          setShowModal((open) => !open);
-                        }}
-                        className={` active:bg-text/15  mb-1 flex w-full  cursor-pointer gap-1 rounded-lg  px-3 py-1 transition-all delay-0 duration-200 ease-in-out last:mb-0 hover:bg-text/10 `}
+                <>
+                  <button
+                    className="w-12"
+                    id="dropdown-button"
+                    data-dropdown-toggle="dropdown"
+                    type="button"
+                    ref={menuButtonRef}
+                    onClick={() => {
+                      setShowModal((open) => !open);
+                    }}
+                  >
+                    {showModal ? "X" : "V"}
+                  </button>
+
+                  <DropDown
+                    open={showModal}
+                    dropDownStateChange={(open: any) => setShowModal(open)}
+                    position="left-2 top-10 "
+                    contents={
+                      <ul
+                        className="space-y-1 p-1 text-sm"
+                        aria-labelledby="dropdown-button"
                       >
-                        <a
-                          className={`flex items-center justify-center  gap-2 capitalize `}
+                        <li
                           onClick={() => {
-                            navigate("/suggested-modpacks");
+                            setShowModal((open) => !open);
                           }}
+                          className={` active:bg-text/15  mb-1 flex w-full  cursor-pointer gap-1 rounded-lg  px-3 py-1 transition-all delay-0 duration-200 ease-in-out last:mb-0 hover:bg-text/10 `}
                         >
-                          Suggested Packs
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="20"
-                            height="20"
-                            fill="currentColor"
-                            viewBox="0 0 256 256"
+                          <a
+                            className={`flex items-center justify-center  gap-2 capitalize `}
+                            onClick={() => {
+                              navigate("/suggested-modpacks");
+                            }}
                           >
-                            <path d="M32,72V56a8,8,0,0,1,8-8H216a8,8,0,0,1,8,8V72a8,8,0,0,1-8,8H40A8,8,0,0,1,32,72Zm8,72H216a8,8,0,0,0,8-8V120a8,8,0,0,0-8-8H40a8,8,0,0,0-8,8v16A8,8,0,0,0,40,144Zm112,32H40a8,8,0,0,0-8,8v16a8,8,0,0,0,8,8H152a8,8,0,0,0,8-8V184A8,8,0,0,0,152,176Zm80,8H216V168a8,8,0,0,0-16,0v16H184a8,8,0,0,0,0,16h16v16a8,8,0,0,0,16,0V200h16a8,8,0,0,0,0-16Z"></path>
-                          </svg>
-                        </a>
-                      </li>
-                      <li
-                        onClick={() => {
-                          setShowModal((open) => !open);
-                        }}
-                        className={` active:bg-text/15  mb-1 flex w-full  cursor-pointer gap-1 rounded-lg  px-3 py-1 transition-all delay-0 duration-200 ease-in-out last:mb-0 hover:bg-text/10 `}
-                      >
-                        <a
-                          className={`flex items-center justify-center  gap-2 capitalize `}
+                            Suggested Packs
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="20"
+                              height="20"
+                              fill="currentColor"
+                              viewBox="0 0 256 256"
+                            >
+                              <path d="M32,72V56a8,8,0,0,1,8-8H216a8,8,0,0,1,8,8V72a8,8,0,0,1-8,8H40A8,8,0,0,1,32,72Zm8,72H216a8,8,0,0,0,8-8V120a8,8,0,0,0-8-8H40a8,8,0,0,0-8,8v16A8,8,0,0,0,40,144Zm112,32H40a8,8,0,0,0-8,8v16a8,8,0,0,0,8,8H152a8,8,0,0,0,8-8V184A8,8,0,0,0,152,176Zm80,8H216V168a8,8,0,0,0-16,0v16H184a8,8,0,0,0,0,16h16v16a8,8,0,0,0,16,0V200h16a8,8,0,0,0,0-16Z"></path>
+                            </svg>
+                          </a>
+                        </li>
+                        <li
                           onClick={() => {
-                            navigate("/archived-modpacks");
+                            setShowModal((open) => !open);
                           }}
+                          className={` active:bg-text/15  mb-1 flex w-full  cursor-pointer gap-1 rounded-lg  px-3 py-1 transition-all delay-0 duration-200 ease-in-out last:mb-0 hover:bg-text/10 `}
                         >
-                          Archived Packs
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="20"
-                            height="20"
-                            fill="currentColor"
-                            viewBox="0 0 256 256"
+                          <a
+                            className={`flex items-center justify-center  gap-2 capitalize `}
+                            onClick={() => {
+                              navigate("/archived-modpacks");
+                            }}
                           >
-                            <path d="M224,48H32A16,16,0,0,0,16,64V88a16,16,0,0,0,16,16v88a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V104a16,16,0,0,0,16-16V64A16,16,0,0,0,224,48Zm-72,96H104a8,8,0,0,1,0-16h48a8,8,0,0,1,0,16Zm72-56H32V64H224V88Z"></path>
-                          </svg>
-                        </a>
-                      </li>
-                    </ul>
-                  }
-                />
+                            Archived Packs
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="20"
+                              height="20"
+                              fill="currentColor"
+                              viewBox="0 0 256 256"
+                            >
+                              <path d="M224,48H32A16,16,0,0,0,16,64V88a16,16,0,0,0,16,16v88a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V104a16,16,0,0,0,16-16V64A16,16,0,0,0,224,48Zm-72,96H104a8,8,0,0,1,0-16h48a8,8,0,0,1,0,16Zm72-56H32V64H224V88Z"></path>
+                            </svg>
+                          </a>
+                        </li>
+                      </ul>
+                    }
+                  />
+                </>
               )}
             </div>
-            {/* Show this button if you're logged in and a staff member */}
 
             <input
               className={`z-10  h-9 w-80 rounded-full border-2 bg-bg  px-3 py-1 text-sm`}
