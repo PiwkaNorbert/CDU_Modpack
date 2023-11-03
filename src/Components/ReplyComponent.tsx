14c1d0d77e2b284b6ebb2f28800379ed74ec1133
PiwkaNorbert <piwkanorbert@gmail.com>
2023-11-03 17:52:13 +0100

    log comment delete

--- src/Components/ReplyComponent.tsx
+++ src/Components/ReplyComponent.tsx
@@ -106,10 +106,12 @@ export function ReplyComponent({
                   disabled={deleteCommentMutation.isLoading}
                   className={` rounded-md border border-sec px-3 py-1 text-justify text-xs text-red-500 hover:border-opacity-20 hover:bg-sec hover:bg-opacity-20  dark:hover:bg-hover-2 `}
                   onClick={async () => {
-                    const commentId = comment?.uuid as string;
                     
                     if (deleteCommentMutation.isLoading) return;
                     if (confirm("Are you sure you want to delete this comment?\n'OK' to confirm")) {
+                      const commentId = comment?.uuid as string;
+                      console.log(commentId);
+                      
                       deleteCommentMutation.mutate(commentId);
                     }
                     else {
