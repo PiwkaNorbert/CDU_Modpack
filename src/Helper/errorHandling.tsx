aa0859d0f4b858e09aa1b6dcf3778fe4ade65556
Norbert Piwka <piwkanorbert@gmail.com>
2023-11-07 07:59:45 +0100

    erroir

--- src/Helper/errorHandling.tsx
+++ src/Helper/errorHandling.tsx
@@ -1,14 +1,19 @@
 import { toast } from "react-toastify";
 
 export const errorHandling = (error: any) => {
-  switch (error.status) {
-    case 401:
-      return toast.error(error.response.data.error);
-    case 404:
-      return toast.error(error.message);
-    default: {
-      console.error(error);
-      return toast.error(error.message);
-    }
-  }
+  if (error.status === 500) return toast.error("Internal Server Error");
+  if (error.status === 400) return toast.error(error.response.data.error);
+  if (error.status === 401) return toast.error(error.response.data.error);
+  if (error.status === 404) return toast.error(error.message);
+
+  // switch (error.status) {
+  //   case 401:
+  //     return toast.error(error.response.data.error);
+  //   case 404:
+  //     return toast.error(error.message);
+  //   default: {
+  //     console.error(error);
+  //     return toast.error(error.message);
+  //   }
+  // }
 };
