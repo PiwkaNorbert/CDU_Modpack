6845b465bb3c4b836b832ee675a810e67fbf8914
PiwkaNorbert <piwkanorbert@gmail.com>
2023-12-21 13:41:40 +0100

    font correction on comments

--- src/Pages/PackDetails.tsx
+++ src/Pages/PackDetails.tsx
@@ -318,7 +318,7 @@ const PackDetails = ({ category }: { category: string }) => {
       {/* comment component here */}
       {category !== "suggested" && (
         <div className="my-4 overflow-hidden py-4 sm:p-4  ">
-          <h3 className="mb-4 inline-block items-center w-full gap-4 text-2xl capitalize xl:text-3xl text-center sm:text-left">
+          <h3 className="mb-4 inline-block items-center text-pri w-full gap-4 text-2xl capitalize xl:text-3xl text-center sm:text-left">
             comments ({commentCount})
             {fetchStatus === "fetching" && (
               <Loading size="la-sm" fullScreen={false} other="inline-block" />
